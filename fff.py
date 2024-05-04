1c403af3da0b34464ed7f0a54d6b46e0c9eebce9
Abbos <abbosyuldashev640@gmail.com>
2024-05-04 10:06:10 +0500

    Update fff.py
    led light range

--- fff.py
+++ fff.py
@@ -1,17 +1,17 @@
 import controller as cnt
 import time
 def fcfs_scheduling(burst_time):
-  
+    
     n = len(burst_time)
     waiting_time = [0] * n
-    process_order = list(range(n))  # FCFS execution order is always arrival order
+    process_order = list(range(n))  
 
-    t = 0  # Current time
+    t = 0
 
     for i in range(n):
         t += burst_time[i]
-        waiting_time[i] = t - burst_time[i]  # Waiting time for each process
-    for i in range(4):
+        waiting_time[i] = t - burst_time[i] 
+    for i in range(len(process_order)):
         cnt.led2(process_order[i])
         time.sleep(1.5)
     return waiting_time, process_order
